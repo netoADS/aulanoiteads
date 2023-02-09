dc1a76d1d0cafe52bd457a70fe9db01490df6f64
netoADS <124839031+netoADS@users.noreply.github.com>
2023-02-09 00:45:33 +0000

    080220232151

---
+++ exe02.py
@@ -0,0 +1,5 @@
+x = " xxxx"
+x3 = "xxxx"
+x2 = "x  x"
+
+print(x,"\n",x2,"\n",x2,"\n",x2,"\n",x3)
---
+++ exe03.py
@@ -0,0 +1,8 @@
+text1 = "Aluno(a)"
+text2 = "   Nota"
+inp1 = input("Nome do aluno:  ")
+inp2 = float(input("Nota: "))
+
+
+
+print(text1, text2, "\n=========", "  ======\n", inp1, "    ", inp2)
