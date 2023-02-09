f00b7ccf75e373ba9a18c4c86e4382179d205500
netoADS <124839031+netoADS@users.noreply.github.com>
2023-02-09 04:17:15 +0000

    090220230116

--- exe02.py
+++ exe02.py
@@ -6,12 +6,3 @@ for b in range(3):
 
 for c in range(1):
     print(('XXXXX').center(10))
-
-#base da árvore
-#print('XXXX'.center(19))
-
-#x = " xxxx"
-#x3 = "xxxx"
-#x2 = "x  x"
-
-#print(x,"\n",x2,"\n",x2,"\n",x2,"\n",x3)
